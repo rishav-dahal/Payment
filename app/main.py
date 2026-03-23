47986c2a83055f5ae935b900692ee52841393278
Rishav <academicdahal21@gmail.com>
2026-03-23 21:28:25 +0545

    chore: update main.py , requirements and add structure file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Payment Service API",
+    description="API for Unified Payments - Manage payment transactions and processors",
+    version="1.0.0",
+)
 
 
 @app.get("/")
